73c967b68567e32cdfda9a0d024c54505ddd4074
Barry <dl-ks@tutanota.com>
2020-01-20 14:18:19 +0000

    Adds response models

---
+++ DistilledTV/DistilledTV/Service/Entity.swift
@@ -0,0 +1,53 @@
+//
+//  File.swift
+//  DistilledTV
+//
+//  Created by Barry on 20/01/2020.
+//  Copyright © 2020 dl-ks. All rights reserved.
+//
+
+import Foundation
+
+struct TvShow: Codable {
+    var posterPath: String
+    var popularity: Int
+    var id: Int
+    var overview: String
+    var originCountry: String
+    var name: String
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        posterPath = try container.decode(String.self, forKey: .posterPath)
+        popularity = try container.decode(Int.self, forKey: .popularity)
+        id = try container.decode(Int.self, forKey: .id)
+        overview = try container.decode(String.self, forKey: .overview)
+        originCountry = try container.decode(String.self, forKey: .originCountry)
+        name = try container.decode(String.self, forKey: .name)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case posterPath = "poster_path"
+        case popularity
+        case id
+        case overview
+        case originCountry = "origin_country"
+        case name
+    }
+}
+
+struct PopularTvShows: Codable {
+    
+    let page: Int
+    let results: [TvShow]
+    let totalResults: Int
+    let totalPages: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case page
+        case results
+        case totalResults = "total_results"
+        case totalPages = "total_pages"
+    }
+    
+}
